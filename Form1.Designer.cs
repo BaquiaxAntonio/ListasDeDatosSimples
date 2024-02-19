fcf4274848229dc5fe9f799d0b53fe861eb86fcf
Henry Baquiax <henrybaquiax@umes.edu.gt>
2024-02-19 09:38:52 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ListasDeDatosSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNoIngresado = new System.Windows.Forms.TextBox();
+            this.buttonIngresar = new System.Windows.Forms.Button();
+            this.buttonMostrar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonOrdenar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un numero ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxNoIngresado
+            // 
+            this.textBoxNoIngresado.Location = new System.Drawing.Point(206, 66);
+            this.textBoxNoIngresado.Name = "textBoxNoIngresado";
+            this.textBoxNoIngresado.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNoIngresado.TabIndex = 1;
+            // 
+            // buttonIngresar
+            // 
+            this.buttonIngresar.Location = new System.Drawing.Point(374, 62);
+            this.buttonIngresar.Name = "buttonIngresar";
+            this.buttonIngresar.Size = new System.Drawing.Size(75, 23);
+            this.buttonIngresar.TabIndex = 2;
+            this.buttonIngresar.Text = "Ingresar";
+            this.buttonIngresar.UseVisualStyleBackColor = true;
+            this.buttonIngresar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonMostrar
+            // 
+            this.buttonMostrar.Location = new System.Drawing.Point(374, 92);
+            this.buttonMostrar.Name = "buttonMostrar";
+            this.buttonMostrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonMostrar.TabIndex = 3;
+            this.buttonMostrar.Text = "Mostrar";
+            this.buttonMostrar.UseVisualStyleBackColor = true;
+            this.buttonMostrar.Click += new System.EventHandler(this.buttonMostrar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(206, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // buttonOrdenar
+            // 
+            this.buttonOrdenar.Location = new System.Drawing.Point(374, 122);
+            this.buttonOrdenar.Name = "buttonOrdenar";
+            this.buttonOrdenar.Size = new System.Drawing.Size(123, 23);
+            this.buttonOrdenar.TabIndex = 5;
+            this.buttonOrdenar.Text = "Ordenar Asendente";
+            this.buttonOrdenar.UseVisualStyleBackColor = true;
+            this.buttonOrdenar.Click += new System.EventHandler(this.buttonOrdenar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Eliminar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(374, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Ordenar Desendente";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonOrdenar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonMostrar);
+            this.Controls.Add(this.buttonIngresar);
+            this.Controls.Add(this.textBoxNoIngresado);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lista de Datos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNoIngresado;
+        private System.Windows.Forms.Button buttonIngresar;
+        private System.Windows.Forms.Button buttonMostrar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonOrdenar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListasDeDatosSimples
+{
+    public partial class Form1 : Form
+    {
+        List<int> listaEnteros = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = 0;
+            bool seConvirtio = int.TryParse(textBoxNoIngresado.Text, out numero);
+            if (seConvirtio)
+            {
+                listaEnteros.Add(numero);
+                textBoxNoIngresado.Text = "";
+                Mostrar();
+            }
+            else
+            {
+                MessageBox.Show("debe ingresar un numero");
+                textBoxNoIngresado.Text = "";
+                textBoxNoIngresado.Select();
+            }
+
+            //textBoxNoIngresado.Text = "";
+        }
+
+        private void Mostrar()
+        {
+            //for (int i = 0; i < listaEnteros.Count; i++)
+            //{
+            //    listBox1.Items.Add(listaEnteros[i]);
+            //}
+
+            //opcion 2:
+            //funciona con: combobox,listBox,dategridview
+            listBox1.DataSource = null;
+            listBox1.DataSource = listaEnteros;
+            listBox1.Refresh();
+        }
+
+        private void buttonMostrar_Click(object sender, EventArgs e)
+        {
+            Mostrar();
+        }
+
+        private void buttonOrdenar_Click(object sender, EventArgs e)
+        {
+            listaEnteros.Sort();
+            Mostrar();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int numero = 0;
+            bool seConvirtio = int.TryParse(textBoxNoIngresado.Text, out numero);
+            if (seConvirtio)
+            {
+                listaEnteros.Remove(numero);
+                Mostrar();
+            }
+            else
+            {
+                MessageBox.Show("debe ingresar un numero");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listaEnteros.Sort();
+            listaEnteros.Reverse();
+            Mostrar();
+        }
+    }
+}
